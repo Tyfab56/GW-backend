ef3c8000a8dc34685491603b49326f6077525138
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-24 13:09:10 +0400

    build

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth', 'admin'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/hotels', [HotelController::class, 'index'])->name('hotels.index');
     Route::get('/hotels/create', [HotelController::class, 'create'])->name('hotels.create');
     Route::post('/hotels', [HotelController::class, 'store'])->name('hotels.store');
