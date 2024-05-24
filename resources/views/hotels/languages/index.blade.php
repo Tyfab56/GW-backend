91783071b518647b44add8fd619bd998654e625b
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-24 21:59:52 +0400

    langue

--- resources/views/hotels/languages/index.blade.php
+++ resources/views/hotels/languages/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container">
-    <h1>Manage Languages for {{ $hotel->name }}</h1>
+    <h1>Manage Languages for {{ $hotel->title }}</h1>
     <form method="POST" action="{{ route('hotels.languages.store', $hotel) }}">
         @csrf
         <div class="form-group">
-            <label for="languages">Select Languages</label>
+            <label for="languages">Choix des langues</label>
             <select class="form-control" id="languages" name="languages[]" multiple>
                 @foreach($languages as $language)
                 <option value="{{ $language->id }}" {{ in_array($language->id, $hotelLanguages) ? 'selected' : '' }}>
@@ -15,7 +15,7 @@
                 @endforeach
             </select>
         </div>
-        <button type="submit" class="btn btn-primary">Save Languages</button>
+        <button type="submit" class="btn btn-primary">Sauver Langues</button>
     </form>
 </div>
 @endsection
