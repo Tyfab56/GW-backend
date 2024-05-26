88f9bb9490981fb78e0821fa41a5efa835a03343
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-26 20:00:33 +0400

    #

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('apihotelsmain/{pseudo}', [HotelController::class, 'show']);
+Route::get('hotel/{pseudo}', [HotelController::class, 'show']);
